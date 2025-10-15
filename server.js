16950bd5d10980c878099a9a34a41ded8cd3f7bb
Nurahmed Jibril <nurahmedjibril@tsehaybank.com.et>
2025-10-15 17:14:11 +0300

    updating

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+
 
 const app = express();
 const port=8000;
+app.use(express.json());
+const STATUS={
+    success:'OK',
+    failure:'NO'
+}
 
 app.get('/a',(req,res)=>{
     res.send(' Hello Nura!');
 });
 
+app.post('/add', (req,res) =>{
+    try {
+        const data = [];
+        data.push(req.body);
+        res.status(StatusCodes.CREATED).send({
+          status:STATUS.success,
+          data: req.body
+    });
+}catch(error){
+    res.status(StatusCodes.BAD_REQUEST).send({
+        status:STATUS.failure,
+        error:'Invalid JSON data'
+    });
+}
+})
 app.listen(port, ()=>{
     console.log(`server started on port : ${port}`);
 });
